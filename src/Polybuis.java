5208219a509112932411d70b754dcdc6d5b4f483
Jake Gadaleta (work) <jake.gads@gmail.com>
2018-09-06 09:52:34 -0400

    Sync

--- src/Polybuis.java
+++ src/Polybuis.java
@@ -5,13 +5,43 @@ import javax.swing.JOptionPane;
  * @author Jake Gadaleta
  *
  */
+/*
+ * String
+			keyword = JOptionPane.showInputDialog("Enter the Key Word");
+		int
+			poly[][],
+			i = 0;
+		
+		for(int counter = 0; counter < keyword.length(); counter++) {
+			if(counter % 5 == 0) {
+				i = 0;
+			}
+ */
 public class Polybuis {
 	public static void main(String[] args) {
-		String
-			keyword = JOptionPane.showInputDialog("Enter the Key Word"),
-			poly;
-		
+			int choice = Integer.parseInt(JOptionPane.showInputDialog("1. Encrypt\n2. Decrypt\n3. Exit"));
+			switch(choice) {
+			case 1: encrypt(); break;
+			case 2: decrypt(); break;
+			case 3: System.exit(0);
+			default:
+				JOptionPane.showMessageDialog(null, "Improper Input", "ERROR", 0);
+		}
+	}
+	
+	private static void decrypt() {
 		
 	}
 
+	private static void encrypt() {
+		String
+			keyword = JOptionPane.showInputDialog("Enter the Key Word"),
+			plainText = JOptionPane.showInputDialog("Enter the plain text");
+		char
+			poly[][];
+		int
+			col = 0,
+			row = 0;
+	}
 }
+
